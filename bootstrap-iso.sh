b53cfd8025d01be537e7c9bc841fa70331c451f6
msaong <morlen911@gmail.com>
2017-08-15 09:22:58 +0800

    Update bootstrap-iso.sh
    fix issue in debian as lack of dirmngr and gnupg
    "gpg: failed to start the dirmngr '/usr/bin/dirmngr': No such file or directory"
    "E: gnupg, gnupg2 and gnupg1 do not seem to be installed, but one of them is required for this operation"

--- bootstrap-iso.sh
+++ bootstrap-iso.sh
@@ -10,6 +10,8 @@ elif [ -f /usr/bin/apt-get ]; then
   DEBIAN_FRONTEND=noninteractive
   mkdir -p /etc/apt/sources.list.d
   echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' > /etc/apt/sources.list.d/ansible.list
+  apt-get -y install dirmngr
+  apt-get -y install gnupg
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
   apt-get update
   apt-get -y install ansible git
