f4faa5d62360780ee2c78d65324c36763a6ff3a4
msaong <morlen911@gmail.com>
2017-08-16 08:10:07 +0800

    Update bootstrap-system.sh
    yum -y install epel-release is no need in fedora, it only need for centos

--- bootstrap-system.sh
+++ bootstrap-system.sh
@@ -50,7 +50,6 @@ elif [ -f /usr/bin/apt-get ]; then
   apt-get -y install ansible git
 elif [ -f /usr/bin/yum ]; then
   yum -y update
-  yum -y install epel-release
   yum -y install dnf
   yum -y install ansible git
 elif [ -f /usr/sbin/emerge ]; then
